85e763243d8b53cd67918c8c37b36ff7d6833154
Nikita Zalizniak <novieasdf@gmail.com>
2024-05-13 16:08:35 +0300

    create NoteEdit component

--- src/views/NoteEdit/NoteEdit.vue
+++ src/views/NoteEdit/NoteEdit.vue
@@ -1,9 +1,33 @@
 <script>
-export default {}
+import { StyledButton } from '@/components'
+export default {
+  components: { StyledButton }
+}
 </script>
 
 <template>
-  <div></div>
+  <div class="edit-card">
+    <label for="note-title">Title</label>
+    <input id="note-title" type="text" placeholder="Please enter title" />
+
+    <label for="note-description">Description</label>
+    <textarea id="note-description" rows="6" placeholder="Please enter description"></textarea>
+
+    <div class="edit-card-bottom">
+      <div>
+        <label for="category-select">Category:</label>
+        <select id="category-select">
+          <option value="default">🏠 Default</option>
+          <option value="work">💼 Work</option>
+          <option value="appointment">🗓️ Appointment</option>
+        </select>
+      </div>
+
+      <StyledButton color="success">Save</StyledButton>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import './NoteEdit.styles.scss';
+</style>
