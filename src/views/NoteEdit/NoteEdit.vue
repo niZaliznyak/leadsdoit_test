22228690d4f69d6bb24ac2a39e2adde9e45a32be
Nikita Zalizniak <novieasdf@gmail.com>
2024-05-13 16:16:10 +0300

    add local state for NoteEdit

--- src/views/NoteEdit/NoteEdit.vue
+++ src/views/NoteEdit/NoteEdit.vue
@@ -1,29 +1,49 @@
 <script>
 import { StyledButton } from '@/components'
+
 export default {
-  components: { StyledButton }
+  components: { StyledButton },
+  data() {
+    return {
+      title: '',
+      description: '',
+      category: 'default',
+      isFavorite: false
+    }
+  },
+
+  methods: {
+    saveNote() {
+      console.log('note is saved')
+    }
+  }
 }
 </script>
 
 <template>
   <div class="edit-card">
     <label for="note-title">Title</label>
-    <input id="note-title" type="text" placeholder="Please enter title" />
+    <input id="note-title" type="text" placeholder="Please enter title" v-model="title" />
 
     <label for="note-description">Description</label>
-    <textarea id="note-description" rows="6" placeholder="Please enter description"></textarea>
+    <textarea
+      id="note-description"
+      rows="6"
+      placeholder="Please enter description"
+      v-model="description"
+    ></textarea>
 
     <div class="edit-card-bottom">
       <div>
         <label for="category-select">Category:</label>
-        <select id="category-select">
+        <select id="category-select" v-model="description">
           <option value="default">🏠 Default</option>
           <option value="work">💼 Work</option>
           <option value="appointment">🗓️ Appointment</option>
         </select>
       </div>
 
-      <StyledButton color="success">Save</StyledButton>
+      <StyledButton @click="saveNote" color="success">Save</StyledButton>
     </div>
   </div>
 </template>
